e3ee331cded60fd2ea851c0d67f2618ef28047ec
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2019-12-18 16:51:12 +0300

    trivia..

---
+++ stores/triviadummy.js
@@ -0,0 +1,33 @@
+data = [
+  {
+    level: [
+      {
+        Question: "question num 1",
+        Answers: [
+          { answer: "wrong 1", right: False },
+          { answer: "wrong 2", right: False },
+          { answer: "right", right: True }
+        ]
+      },
+      {
+        Question: "question num 2",
+        Answers: [
+          { answer: "wrong 1", right: False },
+          { answer: "wrong 2", right: False },
+          { answer: "right", right: True }
+        ]
+      },
+      {
+        Question: "question num 3",
+        Answers: [
+          { answer: "wrong 1", right: False },
+          { answer: "wrong 2", right: False },
+          { answer: "right", right: True }
+        ]
+      }
+    ],
+    score: 100
+  }
+];
+
+export default data;
