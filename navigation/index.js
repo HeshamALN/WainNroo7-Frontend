5f8adfdd4dff9cd7326477cb5cb2ba9c441585c5
Hesham Al Nukhailan <halnukhailan@gmail.com>
2019-12-30 17:58:20 +0300

    fixxx

--- navigation/index.js
+++ navigation/index.js
@@ -2,9 +2,9 @@ import { createAppContainer } from "react-navigation";
 
 // import AuthStack from "./AuthStack";
 import HomeStack from "./HomeStack";
-// import SwitchNav from "./SwitchNav";
+import SwitchNav from "./SwitchNav";
 // const AppContainer = createAppContainer(AuthStack);
-const AppContainer = createAppContainer(HomeStack);
-// const AppContainer = createAppContainer(SwitchNav);
+// const AppContainer = createAppContainer(HomeStack);
+const AppContainer = createAppContainer(SwitchNav);
 
 export default AppContainer;
